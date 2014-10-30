d136cadad0f517f79a66f4344e7b68897de74070
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-10-30 08:26:20 -0400

    Add the module utils
    The module utils contains all our utility functions

---
+++ test_utils.py
@@ -0,0 +1,22 @@
+'''
+Created on 2014-10-30
+
+@author: didia
+'''
+import unittest
+import datetime
+import utils
+
+class TestUtils(unittest.TestCase):
+
+
+    def testGet_week_start_and_end(self):
+        monday, sunday = utils.get_week_start_and_end(today=datetime.datetime(2014,10,29))
+       
+        self.assertEqual(datetime.datetime(2014,10,27), monday)
+        self.assertEqual(datetime.datetime(2014,11,2), sunday)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-10-30
+
+@author: didia
+'''
+import datetime
+
+def get_week_start_and_end(today = None):
+    '''
+    Return a tuple consisting of the current week's monday datetime 
+    and sunday datetime
+    '''
+    date = today or datetime.datetime.today()
+    weekday = date.weekday()
+    monday = date - datetime.timedelta(days=weekday)
+    saturday = date + datetime.timedelta(days=6-weekday)
+    
+    return monday, saturday
