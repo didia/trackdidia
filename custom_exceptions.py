ba542abbf3a79dccc53e4f03760b992975d5b61d
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-10-30 22:13:09 -0400

    Add a module for Custom Exceptions
    This module will contain all user defined exceptions

---
+++ custom_exceptions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-10-30
+
+@author: didia
+'''
+
+class BadArgumentError(Exception):
+    pass
+
+class SlotAlreadyUsed(Exception):
+    pass
