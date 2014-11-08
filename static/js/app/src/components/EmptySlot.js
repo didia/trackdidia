2ac23c346e90ac5a6bd479c9b8432964ecb71311
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-11-07 19:17:29 -0500

    Added a skeleton of an EmptySlot

---
+++ static/js/app/src/components/EmptySlot.js
@@ -0,0 +1,33 @@
+ /** @jsx React.DOM */
+ /**
+ * @author Thefuture2092
+ *
+ */
+
+"user strict";
+
+define(["react"], function(React){
+
+	var EmptySlotComponent = React.createClass({
+		getInitialState : function() {
+
+		},
+
+		componentDidMount: function() {
+
+		},
+
+		componentWillUnmount: function() {
+
+		},
+
+		render: function() {
+			return (
+				<div>
+				</div>
+				);
+		}
+	});
+
+	return EmptySlotComponent;
+})
