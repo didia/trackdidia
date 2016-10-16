edc1bcddf5955172f0b77e05f181578861396e33
Didia Aristote <thefuture2092@gmail.com>
2016-10-16 00:52:45 -0400

    Remove snapshots. Removing snapshots tests as it is not supported in React 15.3. It should be added in React 15.4.0

--- web-client/src/components/App/__tests__/App.test.js
+++ web-client/src/components/App/__tests__/App.test.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-it('has not unexpectedly changed', () => {
-  const component = renderer.create(
-      <App />
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- web-client/src/components/Home/__tests__/Home.test.js
+++ web-client/src/components/Home/__tests__/Home.test.js
@@ -4,17 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from '../Home';
-import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Home />, div);
-});
-
-it('has not unexpectedly changed', () => {
-    const component = renderer.create(
-        <Home />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
 });
